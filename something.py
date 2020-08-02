761436b176c463883661b4db423b5ab1b6f199f4
Brian Okken <1568356+okken@users.noreply.github.com>
2020-08-01 19:00:32 -0700

    initial commit

---
+++ something.py
@@ -0,0 +1,16 @@
+"""
+Well. This is something.
+"""
+
+__version__ = '1.0.0'
+
+def something():
+   """
+   This is something. But not something interesting.
+   """
+   print("This is something. But not something interesting.")
+
+
+if __name__ == "__main__":
+  something()
+   
